228929311a1b2702cc787e5f5a2a8f3f8b999c37
nsatvik <nsatvik@gmail.com>
2013-05-21 10:38:17 +0530

    Some more Stuff

--- location_finder.py
+++ location_finder.py
@@ -1,18 +1,40 @@
 import sys
 
 class search_query:
+	
 	words = []
-	stop_words = []
 	locations = []
 	def __init__(self, query):
 		self.words = query.split(' ')
-		print self.words
+		self.locations = []
 	
-	def findLocations(self):
-		print 'To be Implemented'
+	def findProbableLocations(self,list_of_words):
+		for i in range(len(self.words)):
+			word = self.words[i].lower()
+			
+			if word in list_of_words:
+				print word,'in dictionary'
+				continue
+			self.locations.append((i,word))
+			
+		
 	
-	def correct_location(self):
-		print 'To be implemented'
+	def correct_location(self,list_of_locations,hashmap):
+		# 1 If it is found in the list_of_locations, check the surrounding words update that list
+		# 2 IF not found Check in the hashmap and get the list of probable places. Apply Edit-distance Algo and find the most similar one.
+		#   and update the surrounding words
+		# 3 Add the location 
+		l = len(self.locations)
+		if l > 1: #More that one location in the query! :(
+			pass#Check in the neighbouring words list and take a decision for each word...
+		if l==1:
+			#This is the location and update the neighbouring words list.
+			pass
+		keys = self.get_location_keys()
+		
+		#With these keys find a probable list of locations
+		#If more than 1 locations are mapped use edit-distance or someother spell check and map it to the closest algo :D
+		
 	
 	def toString(self):
 		print 'Coming Soon'
@@ -32,15 +54,41 @@ def initSearchQueries(query_file_name):
 		queries.append(query)
 	return queries
 
-def initLocations(locations_file_name):
-	locations = []
+def file_reader(file_name):
+	words = {}
 	try:
-		locations = open(locations_file_name).read().split('\n')
+		words = open(file_name).read().split('\n')
 	except IOError:
-		print locations_file_name,' : File not found'
+		print file_name,' : File not found'
 		pass
-	return locations
+	return words
 
+def build_location_hashmap(list_of_locations):
+	map = {}
+	
+	for location in list_of_locations:
+		try:
+			map[location[:3]].add(location)
+		except KeyError:
+			map[location[:3]] = {location}
+		try:
+			map[location[-4:]].add(location)
+		except KeyError:
+			map[location[-4:]] = {location}
+		except IndexError:
+			pass
+		try:
+			map[location[3:]].add(location)
+		except KeyError:
+			map[location[3:]] = {location}
+		try:
+			map[location[:2]].add(location)
+		except KeyError:
+			map[location[:2]] = {location}
+		try:
+			map[location[-2:]].add(location)
+		except KeyError:
+			map[location[-2:]] = {location}
 def main():
 	if (len(sys.argv) < 3):
 		print 'Usage python location_finder.py query_file_name locations_file_name'
@@ -48,12 +96,15 @@ def main():
 	
 
 	queries = initSearchQueries(sys.argv[1])
-	locations = initLocations(sys.argv[2])
+	locations = set(file_reader(sys.argv[2]))
 
-	search_query.locations = locations #This is a class variable. This list will be initialized once and will be used by all quries.
-
-
+	list_of_locations = locations#This is a class variable. This list will be initialized once and will be used by all quries.
+	list_of_words = set(file_reader('list_of_words')) - locations
+	hashmap = build_location_hashmap(list_of_locations)
+	print len(list_of_words)
 	
+	for query in queries:
+		query.findProbableLocations(list_of_words)
 
 
 
